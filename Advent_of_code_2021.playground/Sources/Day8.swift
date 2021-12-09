f33e283937d6e544510938791e97161aef415c47
Marcel Śliwiński <marcel.sliwinski@gmail.com>
2021-12-09 17:20:52 +0100

    Day 8 part 2 done

--- Advent_of_code_2021.playground/Sources/Day8.swift
+++ Advent_of_code_2021.playground/Sources/Day8.swift
@@ -8,17 +8,97 @@ extension AOC_2021 {
         }
         
         struct SevenSegmentDisplay {
-            public static func isOne(segments: String) -> Bool {
-                segments.count == 2 // Simple one, unique number of segments
+            internal init(one: String, seven: String, four: String, eight: String) {
+                self.one = one
+                self.seven = seven
+                self.four = four
+                self.eight = eight
             }
-            public static func isFour(segments: String) -> Bool {
-                segments.count == 4
+
+            // Segments go:
+            //   000
+            //  1   2
+            //  1   2
+            //   333
+            //  4   5
+            //  4   5
+            //   666
+            //
+            // Numbers expressed using those segments:
+            // 0: 0,1,2,4,5,6
+            // 1: 2,5
+            // 2: 0,2,3,4,6
+            // 3: 0,2,3,5,6
+            // 4: 1,2,3,5
+            // 5: 0,1,3,5,6
+            // 6: 0,1,3,4,5,6
+            // 7: 0,2,5
+            // 8: 0,1,2,3,4,5,6
+            // 9: 0,1,2,3,5,6
+            //
+            //
+            // Known:
+            // 1: 2,5
+            // 7: 0,2,5
+            // 4: 1,2,3,5
+            // 8: 0,1,2,3,4,5,6 - can ignore, it's just all segments
+            //
+            // Unknown
+            // 3: 0,2,3,5,6 - those segments - "1" symbols == 3 segments; else must be 2 or 5
+            // 2: 0,2,3,4,6 - those segments - ("4" segments - "1" segments) == 4 segments
+            // 5: 0,1,3,5,6 -               else this
+            // 6: 0,1,3,4,5,6 - those segments - "1" segments == 5 segments; else (because 5 segments left) must be either 0 or 9
+            // 0: 0,1,2,4,5,6 - those segments - "4" segments == 3 segments
+            // 9: 0,1,2,3,5,6 - those segments - "4" segments == 2 segments
+            
+            public func getDigit(jumbled: String) -> String {
+                let digit = String(jumbled.sorted())
+                if digit == one { return "1" }
+                if digit == seven { return "7" }
+                if digit == four { return "4" }
+                if digit == eight { return "8" }
+                
+                if digit.count == 5 {
+                    // Must be 2,3 or 5
+                    if digit.filter({!one.contains($0)}).count == 3 {
+                        return "3"
+                    } else if digit.filter({!four.filter{!one.contains($0)}.contains($0)}).count == 4 {
+                        return "2"
+                    } else {
+                        return "5"
+                    }
+                } else {
+                    // Must be 0, 6 or 9
+                    if digit.filter({!one.contains($0)}).count == 5 {
+                        return "6"
+                    } else if digit.filter({!four.contains($0)}).count == 3 {
+                        return "0"
+                    } else {
+                        return "9"
+                    }
+                }
             }
-            public static func isSeven(segments: String) -> Bool {
-                segments.count == 3
+            
+            // Known
+            private var _one: String = ""
+            var one: String {
+                get {_one}
+                set {_one = String(newValue.sorted())}
             }
-            public static func isEight(segments: String) -> Bool {
-                segments.count == 7
+            private var _seven: String = ""
+            var seven: String {
+                get {_seven}
+                set {_seven = String(newValue.sorted())}
+            }
+            private var _four: String = ""
+            var four: String {
+                get {_four}
+                set {_four = String(newValue.sorted())}
+            }
+            private var _eight: String = ""
+            var eight: String {
+                get {_eight}
+                set {_eight = String(newValue.sorted())}
             }
         }
         
@@ -35,24 +115,27 @@ extension AOC_2021 {
             }
             
             // Parsed!
-            
-            // Part 1 - count only easy ones, so 1, 4, 7, 8
-            var counted = 0
-            lines.forEach {
-                $0.outputs.forEach {
-                    if SevenSegmentDisplay.isOne(segments: $0)
-                        || SevenSegmentDisplay.isFour(segments: $0)
-                        || SevenSegmentDisplay.isSeven(segments: $0)
-                        || SevenSegmentDisplay.isEight(segments: $0)
-                    {
-                        counted += 1
-                    }
+
+            var sumOfNumbers = 0
+            lines.forEach { line in
+                let sortedSignals = line.uniqueSignals.sorted{$0.count < $1.count}.compactMap{String($0.sorted())}
+                let one = sortedSignals.first(where: {$0.count == 2})!
+                let seven = sortedSignals.first(where: {$0.count == 3})!
+                let four = sortedSignals.first(where: {$0.count == 4})!
+                let eight = sortedSignals.first(where: {$0.count == 7})!
+                print("1: \(one), 7: \(seven), 4: \(four), 8: \(eight)")
+                let sevenSegmentDisplay = SevenSegmentDisplay(one: one, seven: seven, four: four, eight: eight)
+                
+                var outputNumber = ""
+                line.outputs.forEach { output in
+                    let decodedDigit = sevenSegmentDisplay.getDigit(jumbled: output)
+                    print("For output: \(output) decoded string is \(decodedDigit)")
+                    outputNumber = outputNumber + decodedDigit
                 }
+                sumOfNumbers += Int(outputNumber)!
             }
             
-            
-            print("Number of 1, 4, 7 or 8s: \(counted)")
-            
+            print("Sum of output numbers is: \(sumOfNumbers)")
         }
     }
 }
